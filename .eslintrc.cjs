c0ed080f3cae2f61655cf3153e3289daa960b01d
RajendraDamar <rajendra.damar.setiyawan@mail.ugm.ac.id>
2025-09-20 19:07:17 +0700

    chore(lint): add root and package-level ESLint configs

---
+++ .eslintrc.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  env: {
+    node: true,
+    es2022: true,
+    jest: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'warn',
+  },
+};
